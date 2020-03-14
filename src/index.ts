b0d42ace1dae61cb00274f9ce0f8b3fc305720fd
Daven Quinn <dev@davenquinn.com>
2020-03-14 00:50:34 -0500

    Moved towards typescript

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import h from 'react-hyperscript'
+import {path} from 'd3-path'
+
+const BezierComponent = (props)=>{
+  const {instructions: d} = props
+  const p = path()
+  const p1 = [100,100]
+  const p2 = [400,400]
+
+  let c1 = [...p1]
+  c1[0] += 200
+
+  let c2 = [...p2]
+  c2[0] -= 200
+
+  p.moveTo(...p1)
+  p.bezierCurveTo(...c1,...c2,...p2)
+
+  h('path', {
+    d: p.toString(),
+    stroke: "#888888",
+    fill: 'none',
+    strokeWidth: "2px"
+  })
+}
+
+export {BezierComponent}
