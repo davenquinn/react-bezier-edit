12c060c821cdae162d34e897aab1f6fc4ee52d33
Daven Quinn <dev@davenquinn.com>
2020-03-14 03:47:26 -0500

    Somewhat better data model

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import h from 'react-hyperscript'
 import {path} from 'd3-path'
+import {pairs} from 'd3-array'
 
 interface BezierControlPoint {
   x: number,
@@ -16,24 +17,29 @@ interface BezierComponentProps {
   points: BezierPoint[]
 }
 
-const BezierEditComponent = (props: BezierComponentProps)=>{
+function negate(controlPoint: BezierControlPoint): BezierControlPoint {
+  return {x: -controlPoint.x, y: -controlPoint.y}
+}
+
+const BezierPath = (props: BezierComponentProps)=>{
   const {points} = props
-  const p = path()
-  const p1: BezierPoint = {
-    x: 100,
-    y: 100
+
+  let p = path()
+  p.moveTo(points[0].x, points[0].y)
+
+  for (const [p1, p2] of pairs(points)) {
+
+    let c1 = p1.controlPoint ?? {x: 0, y: 0}
+    let c2 = negate(p2.controlPoint) ?? {x: 0, y: 0}
+
+    p.bezierCurveTo(
+      p1.x+c1.x,
+      p1.y+c1.y,
+      p2.x+c2.x,
+      p2.y+c2.y,
+      p2.x,
+      p2.y)
   }
-  const p2: BezierPoint = {x: 400, y: 400}
-
-  let c1: BezierControlPoint = {...p1}
-  c1.x += 200
-
-  let c2: BezierControlPoint = {...p2}
-  c2.x -= 200
-  c2.y -= 50
-
-  p.moveTo(p1.x, p1.y)
-  p.bezierCurveTo(c1.x, c1.y, c2.x, c2.y, p2.x, p2.y)
 
   return h('path', {
     d: p.toString(),
@@ -41,6 +47,16 @@ const BezierEditComponent = (props: BezierComponentProps)=>{
     fill: 'none',
     strokeWidth: "2px"
   })
+}
+
+const BezierEditComponent = (props)=>{
+  const points = [
+    {x: 100, y: 100, controlPoint: {x: 200, y: 0}},
+    {x: 400, y: 400, controlPoint: {x: 200, y: 0}},
+    {x: 500, y: 300, controlPoint: {x: 200, y: 0}}
+  ]
+
+  return h(BezierPath, {points})
 }
 
 export {BezierEditComponent}
