07d20ac888908ade263894bae49ec09a9b6ea5e3
Daven Quinn <dev@davenquinn.com>
2020-03-17 05:56:08 -0500

    Simplify reducer pattern somewhat

--- src/state-manager.ts
+++ src/state-manager.ts
@@ -37,39 +37,36 @@ const voidDispatch = (a: BezierEditAction)=> a
 const BezierCurveContext = createContext<BezierCurve>([])
 const BezierDispatchContext = createContext<typeof voidDispatch>(voidDispatch)
 
-const bezierReducer: BezierReducer = (curve, action)=>{
-  const updateCurve = (spec: Spec<BezierCurve>)=>{
-    return update<BezierCurve>(curve, spec)
-  }
+function updateControlPoint(curve: BezierCurve, action: DragBezierHandle) {
   const {x, y} = action.data
   const {index} = action
-
-  function updateControlPoint(action: DragBezierHandle) {
-    const point = curve[index]
-    const dx = x-point.x
-    const dy = y-point.y
-    const length = Math.hypot(dx,dy)
-    const angle = Math.atan2(dy,dx)*180/Math.PI-180
-    if (Array.isArray(point.controlPoint)) {
-      // disconnected endpoints not handled yet
-    }
-    let spec: Spec<BezierVertexControls> = {angle: {$set: angle}}
-    if (action.polarity == 1) {
-      spec['length1'] = {$set: length}
-      spec['angle'] = {$set: angle+180}
-    } else {
-      spec['length'] = {$set: length}
-    }
-
-    return updateCurve({[index]: {controlPoint: spec}})
+  const point = curve[index]
+  const dx = x-point.x
+  const dy = y-point.y
+  const length = Math.hypot(dx,dy)
+  const angle = Math.atan2(dy,dx)*180/Math.PI-180
+  if (Array.isArray(point.controlPoint)) {
+    // disconnected endpoints not handled yet
+  }
+  let spec: Spec<BezierVertexControls> = {angle: {$set: angle}}
+  if (action.polarity == 1) {
+    spec['length1'] = {$set: length}
+    spec['angle'] = {$set: angle+180}
+  } else {
+    spec['length'] = {$set: length}
   }
 
+  return update(curve, {[index]: {controlPoint: spec}})
+}
+
+const bezierReducer: BezierReducer = (curve, action)=>{
   switch (action.type) {
     case "drag-handle":
-      return updateControlPoint(action)
+      return updateControlPoint(curve, action)
     case "drag-vertex":
-      const spec: Spec<BezierPoint> = {x: {$set: x}, y: {$set: y}}
-      return updateCurve({[index]: spec})
+      const {x, y} = action.data
+      const {index} = action
+      return update(curve, {[index]: {x: {$set: x}, y: {$set: y}}})
   }
 }
 
